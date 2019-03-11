198bf713b49c187b66ab85081180a6fd3afb78fd
Eivind Jahren <eivind.jahren@webstep.no>
2019-03-11 12:46:09 +0100

    Update webviz_plotly for pypi

--- webviz_plotly/setup.py
+++ webviz_plotly/setup.py
@@ -1,8 +1,18 @@
 from setuptools import setup, find_packages
 
+long_description = ""
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='webviz_plotly',
     version='0.0.1',
+    author='Equinor',
+    author_email='ejah@equinor.com',
+    description='Equinor visualizations for plotly.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/equinor/webviz-core/tree/master/webviz_plotly",
     packages=find_packages("src"),
     package_dir={"": "src"},
     test_suite="setup.discover_test_suite",
@@ -14,6 +24,10 @@ setup(
     ],
     tests_require=[
         'pytest', 'mock', 'pycodestyle'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)"
     ],
     zip_safe=False
 )
